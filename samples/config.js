700e314c1483d08723850384ea056c771eb961ac
Theviyanthan Krishnamohan <theviyanthan@outlook.com>
2022-03-08 16:18:36 +0530

    Update samples/config.js

--- samples/config.js
+++ samples/config.js
@@ -1,5 +1,5 @@
 const config = {
-    "clientID": "_K4U2Cs007M9zXmwT_yh7f2edRka",
+    "clientID": "",
     "serverOrigin": "https://dev.api.asgardeo.io/t/sdktests",
     "signInRedirectURL": "http://localhost:5000/login",
     "signOutRedirectURL": "http://localhost:5000",
