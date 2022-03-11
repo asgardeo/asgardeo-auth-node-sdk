5cf683b9e19d4938d82a513bb4addfe488d305e4
Thivi <theviyanthan@outlook.com>
2022-03-11 21:13:00 +0530

    Modify sign in redirect URL

--- samples/asgardeo-express-app/config.js
+++ samples/asgardeo-express-app/config.js
@@ -2,7 +2,7 @@ const config = {
     "clientID": "UERFdY2YdDM73M1FKmzbTdfA_Gga",
     "clientSecret": "BW3wrZ1giCCmeicV7TefdZOFUhca",
     "serverOrigin": "https://dev.api.asgardeo.io/t/thivi",
-    "signInRedirectURL": "http://localhost:3000/login",
+    "signInRedirectURL": "http://localhost:3000/auth/login",
     "signOutRedirectURL": "http://localhost:3000",
     "scope": [ "openid", "profile" ]
 };
